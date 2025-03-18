257c5bbde9a5306fad10221a8164fe7fdc2e98f5
Yulia Ivanova <yuliaiv2206@yandex.ru>
2025-03-18 20:02:46 +1000

    fix: first review

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function addCard(card) {
+function createCard(card, handleDeleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const imageElement = cardElement.querySelector(".card__image");
   const titleElement = cardElement.querySelector(".card__title");
@@ -15,22 +15,22 @@ function addCard(card) {
   imageElement.alt = card.name;
   titleElement.textContent = card.name;
 
-  deleteButton.addEventListener("click", deleteCard);
-  return cardsContainer.append(cardElement);
+  deleteButton.addEventListener("click", handleDeleteCard);
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(e) {
-  const listItem = e.target.closest('.card');
+  const listItem = e.target.closest(".card");
   listItem.remove();
 }
 
 // @todo: Вывести карточки на страницу
 function renderCards(itemList) {
   itemList.forEach((card) => {
-    addCard(card);
+    const cardElement = createCard(card, deleteCard);
+    cardsContainer.append(cardElement);
   });
 }
 
 renderCards(initialCards);
-
