469333100e65fba021b1b8c245ecdb8a720e03f7
Yulia Ivanova <yuliaiv2206@yandex.ru>
2025-03-18 18:05:32 +1000

    feat: render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(card) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const imageElement = cardElement.querySelector(".card__image");
+  const titleElement = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  imageElement.src = card.link;
+  imageElement.alt = card.name;
+  titleElement.textContent = card.name;
+
+  deleteButton.addEventListener("click", deleteCard);
+  return cardsContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(e) {
+  const listItem = e.target.closest('.card');
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards(itemList) {
+  itemList.forEach((card) => {
+    addCard(card);
+  });
+}
+
+renderCards(initialCards);
+
